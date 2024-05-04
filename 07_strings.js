e1f737de390ccdc4763ad195fc137d5dde039a34
Sneha Purkayastha <iamsneha0296@gmail.com>
2024-05-04 09:37:53 +0530

    updated lesson 7

---
+++ 07_strings.js
@@ -0,0 +1,7 @@
+const name = "sneha"
+const repoCount = 80
+console.log(name + " " + repoCount);
+
+
+//string interpolation (template literals)
+console.log(`My name is ${name} and my repoCount is ${repoCount}`);
